13801c57d78fc08ae4ba7243c7a6da8fe43056ae
SashaBlack <marcupan@inbox.ru>
2015-12-27 00:53:13 +0200

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -5,6 +5,11 @@ public class Calculate{
 	public static void main(String[] arg){
 
 		System.out.println("Calculate");
+		int first = Integer.parseInt(arg[0]);
+		int second = Integer.parseInt(arg[1]);
+		int sum = first + second;
+			System.out.println("Sum = " + sum);
+
 
  		}
 }
